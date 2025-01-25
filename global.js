950ea7ddad025f40b676f5fc10a1cc7481d781ab
OMIDALAM01 <oalamdar@ucsd.edu>
2025-01-24 17:46:03 -0800

    new updates lab 3 step 3

--- global.js
+++ global.js
@@ -1,13 +1,41 @@
-console.log('IT’S ALIVE!');
+console.log("IT’S ALIVE!");
 
 function $$(selector, context = document) {
-    return Array.from(context.querySelectorAll(selector));
+  return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+let pages = [
+  { url: "index.html", title: "Home" },
+  { url: "projects/index.html", title: "Projects" },
+  { url: "contact/index.html", title: "Contact" },
+  { url: "resume/index.html", title: "Resume" },
+  { url: "https://github.com/OMIDALAM01/portfolio", title: "GitHub" }
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
-currentLink?.classList.add("current");
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!ARE_WE_HOME && !url.startsWith("http")) {
+    url = "../" + url;
+  }
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}
