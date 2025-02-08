3007f314da863ae15a4d015837886fd86d6dcc5d
OMIDALAM01 <oalamdar@ucsd.edu>
2025-02-07 19:05:10 -0800

    Creating pie chart and slices

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,5 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projectsContainer = document.querySelector('.projects');
@@ -22,3 +24,27 @@ async function updateProjectCount() {
 }
 
 updateProjectCount();
+
+function renderPieChart(data) {
+    const svg = d3.select('#projects-plot');
+    const width = 100;
+    const radius = width / 2;
+
+    const color = d3.scaleOrdinal(d3.schemeTableau10);
+
+    const arcGenerator = d3.arc().innerRadius(0).outerRadius(radius);
+
+    const pie = d3.pie();
+    const pieData = pie(data);
+
+    svg.selectAll('path')
+        .data(pieData)
+        .join('path')
+        .attr('d', arcGenerator)
+        .attr('fill', (d, i) => color(i))
+        .attr('transform', `translate(${radius - 50}, ${radius - 50})`);
+}
+
+const pieData = [1, 2, 3, 4, 5];
+renderPieChart(pieData);
+
