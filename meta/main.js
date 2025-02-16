4f6f8a69a0c3a8b03805b013b8bbccbf98bc41b5
OMIDALAM01 <oalamdar@ucsd.edu>
2025-02-15 16:10:57 -0800

    Updates

--- meta/main.js
+++ meta/main.js
@@ -80,53 +80,92 @@ function displayStats() {
 function createScatterplot() {
     const width = 1000;
     const height = 600;
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
 
-    const svg = d3.select("#chart")
-        .append("svg")
-        .attr("viewBox", `0 0 ${width} ${height}`)
-        .style("overflow", "visible");
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom
+    };
+
+    const svg = d3.select('#chart')
+        .append('svg')
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .style('overflow', 'visible');
 
     const xScale = d3.scaleTime()
         .domain(d3.extent(commits, (d) => d.datetime))
-        .range([0, width])
+        .range([usableArea.left, usableArea.right])
         .nice();
 
     const yScale = d3.scaleLinear()
         .domain([0, 24])
-        .range([height, 0]);
+        .range([usableArea.bottom, usableArea.top]);
 
-    const dots = svg.append("g").attr("class", "dots");
+    const gridlines = svg.append('g')
+        .attr('class', 'gridlines')
+        .attr("transform", `translate(${usableArea.left}, 0)`)
+        .call(d3.axisLeft(yScale).tickFormat("").tickSize(-usableArea.width));
 
-    dots.selectAll("circle")
-        .data(commits)
-        .join("circle")
-        .attr("cx", (d) => xScale(d.datetime))
-        .attr("cy", (d) => yScale(d.hourFrac))
-        .attr("r", 5)
-        .attr("fill", "steelblue");
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale).tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
 
-    const xAxis = d3.axisBottom(xScale).ticks(5);
-    const yAxis = d3.axisLeft(yScale).ticks(10);
-
-    svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
+    svg.append('g')
+        .attr('transform', `translate(0, ${usableArea.bottom})`)
         .call(xAxis);
 
-    svg.append("g")
+    svg.append('g')
+        .attr('transform', `translate(${usableArea.left}, 0)`)
         .call(yAxis);
 
-    svg.append("text")
-        .attr("x", width / 2)
-        .attr("y", height + 40)
-        .attr("text-anchor", "middle")
-        .text("Date");
+    const dots = svg.append('g')
+        .attr('class', 'dots');
 
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -height / 2)
-        .attr("y", -40)
-        .attr("text-anchor", "middle")
-        .text("Time of Day");
+    dots.selectAll('circle')
+        .data(commits)
+        .join('circle')
+        .attr('cx', (d) => xScale(d.datetime))
+        .attr('cy', (d) => yScale(d.hourFrac))
+        .attr('r', 5)
+        .attr('fill', 'steelblue')
+        .on('mouseenter', (event, commit) => {
+            updateTooltipContent(commit);
+            updateTooltipVisibility(true);
+            updateTooltipPosition(event);
+        })
+        .on('mousemove', (event) => {
+            updateTooltipPosition(event);
+        })
+        .on('mouseleave', () => {
+            updateTooltipContent({});
+            updateTooltipVisibility(false);
+        });
+}
+
+// Tooltip functions
+function updateTooltipContent(commit) {
+    const link = document.getElementById('commit-link');
+    const date = document.getElementById('commit-date');
+
+    if (Object.keys(commit).length === 0) return;
+
+    link.href = commit.url;
+    link.textContent = commit.id;
+    date.textContent = commit.datetime.toLocaleString('en', { dateStyle: 'full' });
+}
+
+function updateTooltipVisibility(isVisible) {
+    const tooltip = document.getElementById('commit-tooltip');
+    tooltip.hidden = !isVisible;
+}
+
+function updateTooltipPosition(event) {
+    const tooltip = document.getElementById('commit-tooltip');
+    tooltip.style.left = `${event.clientX + 10}px`;
+    tooltip.style.top = `${event.clientY + 10}px`;
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
